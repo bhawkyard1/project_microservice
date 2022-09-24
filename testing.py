d6d89ad799402f032f1a960de2233e7b3f3715a2
Ben <bhawkyard1@gmail.com>
2022-09-24 18:19:39 +0100

    Add test GET.

--- testing.py
+++ testing.py
@@ -2,3 +2,5 @@ import requests
 
 test_data = {"name": "foo", "path": "/foo/bar/baz"}
 requests.request("POST", "http://localhost:5000/project", json=test_data)
+
+print(requests.request("GET", "http://localhost:5000/project"))
