db51b44e2d8cfe1a55e0a878f9600ee5415b037b
Ben <bhawkyard1@gmail.com>
2022-09-24 18:09:42 +0100

    Use json parameter rather than data in test request.

--- testing.py
+++ testing.py
@@ -1,4 +1,4 @@
 import requests
 
 test_data = {"name": "foo", "path": "/foo/bar/baz"}
-requests.request("POST", "http://localhost:5000/project", data=test_data)
+requests.request("POST", "http://localhost:5000/project", json=test_data)
